829e34d55f73b0351cc1656a22e2db3779c94ac8
kopytjuk <kopytjuk@gmail.com>
2019-11-24 15:28:46 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn 8051
+
+A collection of 8051 EdSim51 programs used to learn about the 8051 architecture.
---
+++ increment-7segment.asm
@@ -0,0 +1,28 @@
+; Increments the 7-segment display from 0 to 9
+; EdSim update frequency: 1s
+
+; put low and high byte of the program memory address of the first 7-segment code into DPTR to use in MOVC command
+MOV DPL, #LOW(LEDcodes)		; | put the low byte of the start address of the
+					; | 7-segment code table into DPL
+MOV DPH, #HIGH(LEDcodes)	; put the high byte into DPH
+
+main:
+    MOV R1, 0
+    loop:
+        MOV A, R1
+        MOVC A, @A+DPTR ; loads a 7-seg code into memory
+        LCALL delay1s
+        INC R1
+        MOV P1, A
+        CJNE R1, #10, loop
+    JMP main
+
+delay1s:
+	MOV R0, #1
+	DJNZ R0, $
+	RET
+
+
+LEDcodes:	; points to the address of the first byte defined below
+    ; 7-segment codes for 0, 1, ..., 9 digits
+	DB 11000000B, 11111001B, 10100100B, 10110000B, 10011001B, 10010010B, 10000010B, 11111000B, 10000000B, 10010000B
